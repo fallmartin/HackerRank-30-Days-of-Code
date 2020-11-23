3c1b674cb86794889dccfab06eac43abb14820bf
Martin Fall <martinfall@gmail.com>
2020-11-23 01:54:18 -0500

    Create Solution.java

---
+++ Day 11: 2D Arrays/Solution.java
@@ -0,0 +1,52 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
+
+        // Calculate the sum of the first hourglass
+        int sum = arr[0][0] + arr[0][1] + arr[0][2]
+                + arr[1][1]
+                + arr[2][0] + arr[2][1] + arr[2][2];
+
+        // Compare the sum to the sums of all remaining sums in the array
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int currentSum
+                        = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                
+                // If the current sum is larger, assign its value to sum
+                if (currentSum > sum) {
+                    sum = currentSum;
+                }
+            }
+        }
+
+        // Display the largest hourglass sum to the console
+        System.out.println(sum);
+    }
+}
