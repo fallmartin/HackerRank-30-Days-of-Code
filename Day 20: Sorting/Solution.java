c01f89638c3fd381103bcd021e96951b9a475351
Martin Fall <martinfall@gmail.com>
2020-11-28 06:30:54 -0500

    Create Solution.java

---
+++ Day 20: Sorting/Solution.java
@@ -0,0 +1,46 @@
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int totalNumberOfSwaps = 0;
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+
+        in.close();
+
+        for (int i = 0; i < a.length; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            } else {
+                totalNumberOfSwaps += numberOfSwaps;
+            }
+        }
+
+        System.out.println("Array is sorted in " + totalNumberOfSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+}
